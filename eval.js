71d3e16cf9cf3a95c55ce34f55e2d8778b0c2250
AadiVishu4 <vishalofiicial268@gmail.com>
2022-06-28 15:52:34 +0530

    eval finished

---
+++ eval.js
@@ -0,0 +1,12 @@
+// console.log("eval file....")
+
+// eval('console.log("I am from Eval")');
+
+// let val = eval('let i = 0; ++i');
+// console.log(val)
+
+// eval("let x = 5; function f() {}");
+// console.log(x) // eval has its own lexical enviorments:
+
+// let myEvalFunction = new Function('x', 'console.log(x)');
+// myEvalFunction(200) // instead of using eval, we can also use this.
